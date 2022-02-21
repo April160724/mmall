b9f37a4c534b25b00bf240670b0d4e681a47cbd6
hukun20160724 <190866228@qq.com>
2022-02-21 10:41:05 +0800

    产品：对应的富文本上传2

--- src/main/java/com/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
